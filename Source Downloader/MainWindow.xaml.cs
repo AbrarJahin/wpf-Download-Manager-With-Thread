1a36295f0fbe1ed67fb9b34de8a9de572e1a60c9
S. M. Abrar Jahin <abrarjahin@live.com>
2016-11-24 14:53:35 +0600

    Current status

--- Source Downloader/MainWindow.xaml.cs
+++ Source Downloader/MainWindow.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Source_Downloader
 {
@@ -19,9 +11,55 @@ namespace Source_Downloader
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string downloadFileLocation = AppDomain.CurrentDomain.BaseDirectory + "download.txt";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void bDownloadClick(object sender, RoutedEventArgs e)
+        {
+            bDownload.IsEnabled = false;
+            String downloadedSourceString = string.Empty;
+
+            // Create a New 'HttpWebRequest' object .
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(downloadUrl.Text);
+            //myHttpWebRequest.AddRange(50, 51);
+
+            // Assign the response object of 'HttpWebRequest' to a 'HttpWebResponse' variable.
+            HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+
+            MessageBox.Show(myHttpWebResponse.ContentLength.ToString());
+
+            // Display the contents of the page to the console.
+            Stream streamResponse = myHttpWebResponse.GetResponseStream();
+            StreamReader streamRead = new StreamReader(streamResponse);
+            Char[] readBuffer = new Char[256];
+            int count = streamRead.Read(readBuffer, 0, 256);
+
+            while (count > 0)
+            {
+                String outputData = new String(readBuffer, 0, count);
+                //Console.WriteLine(outputData);
+                downloadedSourceString += outputData;
+                count = streamRead.Read(readBuffer, 0, 256);
+            }
+
+            // Release the response object resources.
+            streamRead.Close();
+            streamResponse.Close();
+            myHttpWebResponse.Close();
+
+            // Store the result
+            File.WriteAllText(downloadFileLocation, downloadedSourceString);
+
+            //Now calculate total no of divs
+            var matches = Regex.Matches(downloadedSourceString.ToLower(), "</div>");
+            calculationResultValue.Text = matches.Count.ToString();
+
+            bDownload.IsEnabled = true;
+            MessageBox.Show("Download Completed");
         }
     }
 }
